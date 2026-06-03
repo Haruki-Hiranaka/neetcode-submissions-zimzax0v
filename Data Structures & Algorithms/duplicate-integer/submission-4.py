9587bb89f5812f379c8b1b2e0124294b674f9768
平中暖樹 <harukirun@icloud.com>
2026-06-03 14:59:21 +0900

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        visited = {}
+
+        for n in nums:
+            if n in visited:
+                return True
+            else:
+                visited[n] = 1
+        return False
+        
